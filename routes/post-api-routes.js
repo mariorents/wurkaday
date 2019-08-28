d144063ec1e6a0a8cf21f9acdd6341141ffece9d
jake hanes <jhanesx@yahoo.com>
2019-08-27 19:31:55 -0700

    full crud for post api routes made

---
+++ routes/post-api-routes.js
@@ -0,0 +1,63 @@
+// Require models
+var db = require("../models");
+
+// Routes
+
+module.exports = function(app) {
+
+    // GET route for getting all of the posts
+    app.get("/api/posts", function(req, res) {
+      var query = {};
+      if (req.query.profiles_id) {
+        query.ProfilesId = req.query.profiles_id;
+      }
+      db.Post.findAll({
+        where: query
+      }).then(function(dbPost) {
+        res.json(dbPost);
+      });
+    });
+  
+    // Get route for one post
+    app.get("/api/posts/:id", function(req, res) {
+      db.Post.findOne({
+        where: {
+          id: req.params.id
+        }
+      }).then(function(dbPost) {
+        console.log(dbPost);
+        res.json(dbPost);
+      });
+    });
+  
+    // POST route for saving a new post
+    app.post("/api/posts", function(req, res) {
+      db.Post.create(req.body).then(function(dbPost) {
+        res.json(dbPost);
+      });
+    });
+  
+    // DELETE route for deleting posts
+    app.delete("/api/posts/:id", function(req, res) {
+      db.Post.destroy({
+        where: {
+          id: req.params.id
+        }
+      }).then(function(dbPost) {
+        res.json(dbPost);
+      });
+    });
+  
+    // PUT route for updating posts
+    app.put("/api/posts", function(req, res) {
+      db.Post.update(
+        req.body,
+        {
+          where: {
+            id: req.body.id
+          }
+        }).then(function(dbPost) {
+        res.json(dbPost);
+      });
+    });
+  };
