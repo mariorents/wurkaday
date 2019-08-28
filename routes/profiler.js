093d7c862869076bc60b7f3cac01ee546d753171
Mario Renteria <mrente94@gmail.com>
2019-08-27 20:10:16 -0700

    added profiler routes

--- routes/profiler.js
+++ routes/profiler.js
@@ -1,6 +1,7 @@
 const db = require("../models");
 
 module.exports = function(app) {
+    // Route for the getting all the profiles including posts 
     app.get("/api/profiles", (req,res)=> {
         db.Profile.findAll({
             include: [db.Post]
@@ -8,7 +9,29 @@ module.exports = function(app) {
             res.json(dbProfile);
         });
         
+    }),
+
+    app.get("api/profiles/:id", (req,res)=> {
+        db.Profile.findOne({
+            where: {
+                id: req.params.id
+            }
+        }).then(dbProfile => {
+            console.log(dbProfile);
+            res.json(dbProfile);     
+        });
+    })
+
+    app.post("/api/profiles", (req,res)=> {
+        db.Profile.create(req.body).then(dbProf=> res.json(dbProf));
+    }),
+
+    app.delete("/api/profiles/:id", (req, res) => {
+        db.Profile.destroy({
+            where: {
+                id: req.params.id
+            }
+        }).then(deleteProf => res.json(deleteProf));
     });
 
-
-}
+};
