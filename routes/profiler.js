e252092d66c9170ae1eec52b7ea21e266f6550c3
Mario Renteria <mrente94@gmail.com>
2019-08-27 19:44:39 -0700

    little code added

--- routes/profiler.js
+++ routes/profiler.js
@@ -1,8 +1,14 @@
 const db = require("../models");
 
 module.exports = function(app) {
-    app.get("/api/posts", (req,res)=> {
-        const query = {};
-        if()
+    app.get("/api/profiles", (req,res)=> {
+        db.Profile.findAll({
+            include: [db.Post]
+        }).then(dbProfile=> {
+            res.json(dbProfile);
+        });
+        
     });
+
+
 }
