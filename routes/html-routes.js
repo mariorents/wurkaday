4114165eea92c54f762ed4ea321c7283a6b3718b
Mario Renteria <mrente94@gmail.com>
2019-08-29 20:12:17 -0700

    added the html routes with some css

---
+++ routes/html-routes.js
@@ -0,0 +1,16 @@
+const path = require("path");
+
+module.exports = function(app) {
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+    
+    app.get("/profiles", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/profiles.html"));
+    });
+    
+    app.get("/posts", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/posts.html"));
+    });
+    
+};
