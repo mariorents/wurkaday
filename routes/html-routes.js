f3ba3ef585394890f11eeb9bb8935a644ac8ff92
Mario Renteria <mrente94@gmail.com>
2019-09-03 18:41:00 -0700

    working on login

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,15 +1,27 @@
 const path = require("path");
+const isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
+
     app.get("/", function(req, res) {
+        if(req.user) {
+            res.redirect("/home");
+        }
         res.sendFile(path.join(__dirname, "../public/login.html"));
     });
     
+    app.get("/signup", function(req, res) {
+        if(req.user) {
+            res.redirect("/home");
+        }
+        res.sendFile(path.join(__dirname, "../public/signup.html"));
+    });
+
     app.get("/home", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/home.html"));
     });
     
-    app.get("/profiles", function(req, res) {
+    app.get("/profiles", isAuthenticated, function(req, res) {
         res.sendFile(path.join(__dirname, "../public/profiles.html"));
     });
     
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
-
+const passport = require('passport');
+const session = require('express-session');
+const exphbs = require('express-handlebars')
 // Sets up the Express App
 // =============================================================
 const app = express();
@@ -9,8 +11,24 @@ const PORT = process.env.PORT || 8080;
 const db = require("./models");
 
 // Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+  extended: true
+}));
 app.use(express.json());
+app.use(session({
+  secret: "keyboard cat",
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Handlebars 
+app.set('views', './app/views')
+app.engine('hbs', exphbs({
+  extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
 
 // Static directory
 app.use(express.static("public"));
@@ -23,8 +41,10 @@ require("./routes/post-api-routes.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync({ force: true }).then(function() {
-  app.listen(PORT, function() {
+db.sequelize.sync({
+  force: true
+}).then(function () {
+  app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
   });
 });
