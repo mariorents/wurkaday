434cd1528bbc5f855b19baf83e7a0b5179aa6fb2
Sam Estorga <estorgajr@gmail.com>
2019-09-05 20:24:32 -0700

    stufffff

--- public/assets/javascript/forum.js
+++ public/assets/javascript/forum.js
@@ -1,18 +1,13 @@
 $(document).ready(function () {
 
-
     const titleInput = $("#jobTitle");
     const paymentInput = $("#paymentAmount");
     const subjectInput = $("#subjectType");
     const descriptionInput = $("#descriptionInput");
     
-    
-    
-    
     const forumSubmit = (event) => {
         event.preventDefault();
 
-        
         const newPost = {
             title: titleInput.val(),
             body: descriptionInput.val(),
@@ -25,19 +20,13 @@ $(document).ready(function () {
         console.log(newPost);
     }
     
-    
     function submitPost(post) {
         $.post("/api/posts", post, function() {
             window.location.href = "/posts.html";
             console.log(post);
         });
-        
     }
     
-    
     $("#submitBtn").on("click", forumSubmit);
-    
-    
-    
     
 })
