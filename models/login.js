0f4b754d228c8d40c937a7cc4948510d0117736b
Mario Renteria <mrente94@gmail.com>
2019-09-05 17:30:16 -0700

    i think im all done

--- models/login.js
+++ models/login.js
@@ -1,4 +1,5 @@
-const db = require("../models");
+const bcrypt = require("bcrypt");
+
 
 module.exports = (sequelize, DataTypes) => {
     const Users = sequelize.define("Users", {
@@ -7,29 +8,47 @@ module.exports = (sequelize, DataTypes) => {
             primaryKey: true,
             type: DataTypes.INTEGER
         },
- 
+
         username: {
             type: DataTypes.TEXT,
             allowNull: false
         },
-        
+
         profileType: {
             type: DataTypes.TEXT
-                
+
+        },
+
+        about: {
+            type: DataTypes.TEXT
+        },
+
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isEmail: true
+            }
         },
 
         password: {
             type: DataTypes.STRING,
             allowNull: false
         },
+
         
-        status: {
-            type: DataTypes.ENUM("active","inactive"),
-            defaultValue: "active"
-        }
-            
     });
+
+    
+    
+    Users.prototype.validPassword = password => {
+        return bcrypt.compareSync(password, this.password);
+    };
+
+    Users.addHook("beforeCreate", user => {
+        user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+    });
+
+
     return Users;
-}
-
-
+};
