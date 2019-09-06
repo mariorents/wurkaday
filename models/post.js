713372ffd06f2186d945ffc26304868f5aea8229
Mario Renteria <mrente94@gmail.com>
2019-09-05 20:00:18 -0700

    eh

--- models/post.js
+++ models/post.js
@@ -34,7 +34,7 @@ module.exports = (sequelize, DataTypes) => {
       Post.belongsTo(models.Profile, {
         
         foreignKey: {
-          allowNull: false
+          allowNull: true
         }
       });
   
--- models/profiles.js
+++ models/profiles.js
@@ -2,24 +2,10 @@ module.exports = (sequelize, DataTypes) => {
     var Profile = sequelize.define("Profile", {
         name: {
            type: DataTypes.STRING,
-           allowNull: false,
+           allowNull: true,
            validate: {
             len: [1]
            }
-        },
-        description: {
-          type: DataTypes.TEXT,
-          allowNull: false,
-          validate: {
-            len: [1]
-          }
-        },       
-        skills: {
-          type: DataTypes.STRING,
-          allowNull: false,
-          validate: {
-            len: [1]
-          }
         }
     });
     // this should join with the post.js model
